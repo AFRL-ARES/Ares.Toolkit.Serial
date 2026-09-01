46a9a6a1894f4d8504f28df3f0330321c884c8e4
garath001 <62758265+garath001@users.noreply.github.com>
2026-09-01 10:57:42 -0400

    Added stubouts for protocol

---
+++ Ares.Toolkit.Serial/SerialDeviceProtocol.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ares.Toolkit.Serial
+{
+    /// <summary>
+    /// Standard serial protocol identifiers.
+    ///
+    /// Devices may also specify custom protocol identifiers.
+    /// Devices using compatible protocols may share a serial bus.
+    /// </summary>
+    public static class SerialDeviceProtocols
+    {
+        /// <summary>
+        /// Dedicated protocol, where the device has exclusive access to the serial bus.
+        /// </summary>
+        public const string Dedicated = "DEDICATED";
+        public const string ModbusRtu = "MODBUS_RTU";
+        public const string ModbusAscii = "MODBUS_ASCII";
+        public const string ModbusTpu = "MODBUS_TPU";
+    }
+}
--- Ares.Toolkit.Serial/SerialPortConnectionInfo.cs
+++ Ares.Toolkit.Serial/SerialPortConnectionInfo.cs
@@ -4,17 +4,27 @@ namespace Ares.Toolkit.Serial;
 
 public class SerialPortConnectionInfo
 {
-  public SerialPortConnectionInfo(int baudRate, Parity parity, int dataBits,StopBits stopBits)
-  {
-    BaudRate = baudRate;
-    Parity = parity;
-    DataBits = dataBits;
-    StopBits = stopBits;
-  }
+    public SerialPortConnectionInfo(int baudRate, Parity parity, int dataBits, StopBits stopBits)
+    {
+        BaudRate = baudRate;
+        Parity = parity;
+        DataBits = dataBits;
+        StopBits = stopBits;
+    }
 
-  public int BaudRate { get; set; }
-  public Parity Parity { get; set; }
-  public int DataBits { get; set; }
-  public StopBits StopBits { get; set; }
-  public string EndOfInput { get; set; } = string.Empty;
+    public SerialPortConnectionInfo(int baudRate, Parity parity, int dataBits, StopBits stopBits, string protocol)
+    {
+        BaudRate = baudRate;
+        Parity = parity;
+        DataBits = dataBits;
+        StopBits = stopBits;
+        Protocol = protocol;
+    }
+
+    public int BaudRate { get; set; }
+    public Parity Parity { get; set; }
+    public int DataBits { get; set; }
+    public StopBits StopBits { get; set; }
+    public string EndOfInput { get; set; } = string.Empty;
+    public string Protocol { get; set; } = SerialDeviceProtocols.Dedicated;
 }
